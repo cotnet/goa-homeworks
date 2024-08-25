9e818cc40dfb15555319b516b13eb4308c2d6c9b
cotnet <tevdorashvilicotne2010@gmail.com>
2024-08-25 22:16:32 +0400

    first commit

---
+++ day1/leson2.py
@@ -0,0 +1,6 @@
+name="ცოტნე"
+surname="თევდორაშვილი"
+age=13
+height=170
+birth_day="20/10/2010"
+print("მე ვარ " + name + " " + surname + ", ასაკი: " + str(age) + ", სიმაღლე: " + str(height) + ", დაბადების დღე: " + birth_day)
