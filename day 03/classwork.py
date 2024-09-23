dc4bf4bf1f73c3bbecc38123aa1e1de055417e6d
cotnet <tevdorashvilicotne2010@gmail.com>
2024-09-23 21:13:00 +0400

    second commit

---
+++ day 03/classwork.py
@@ -0,0 +1,18 @@
+# classwork 1
+age = 14
+parentsage = 44
+print (parentsage/age)
+
+
+
+
+
+
+
+
+
+
+# classwork 2
+age = 14  #ეხლანდელი
+age = 24  #10 წლის შემდეგ
+print(age)
---
+++ day 03/homework.py
@@ -0,0 +1,62 @@
+1
+# +
+print(136193+3771273)
+print(2681266379+44841848)
+print(1234+5678)
+print(9786+269)
+print(26922182+28181282)
+
+# -
+print(3771273 - 136193)
+print(2681266379 - 44841848)
+print(5678 - 1234)
+print(9786 - 269)
+print(1000000 - 500000)
+
+# *
+print(412471*127714)
+print(28338912*4161489)
+print(261931263892*73873)
+print(11829*1471)
+print(88288*3289)
+
+# /
+print(848349/9732)
+print(9812/9102)
+print(123271702487/313)
+print(2784811/14)
+print(814/21)
+
+
+
+
+
+
+2
+age=14
+age1=19
+print(age1)
+
+
+
+
+3
+mother=44
+father=50
+sister=17
+brother=20
+sister1=9
+print(mother+father+sister+brother+sister1)
+
+
+
+
+
+
+
+4
+name="cotne"
+name="nodo"
+print (name)
+
+
