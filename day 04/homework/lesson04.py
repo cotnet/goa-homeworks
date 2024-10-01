33d62f51ccb0196eebf184ce5e2dd5ee87878352
cotnet <tevdorashvilicotne2010@gmail.com>
2024-10-01 15:22:35 +0400

    second commit

---
+++ day 04/homework/lesson04.py
@@ -0,0 +1,37 @@
+# 1
+# ჩემი სახელი 
+name=input("your name:")
+# მეგობრის გვარი
+friend_last_name=input("your frend last name:")
+# სტრინგების კონკატენაცია
+result = name + " " + friend_last_name
+# დაბეჭდვა
+print (result)
+# 2
+age= int(input("your age: "))
+new_age = age + 10
+print(str(new_age)) 
+#3
+number1=int(input("first number="))
+number2=int(input("second number="))
+number3=int(input("thrid number="))
+number4=int(input("fourth number="))
+number5=int(input("fifth number="))
+number=(number1 + number2 + number3 + number4 + number5)
+print(number/5)
+
+#4
+#Snake case: სახელების ფორმატირების სტილი, სადაც სიტყვები გაწვდილი მაქვს ხაზი (_), ყველა ასო კი პატარა რეგისტრით.
+#Case sensitivity ნიშნავს, რომ სისტემამ ან პროგრამამ არ უნდა გააერთიანოს ან არ უნდა აღიაროს სხვადასხვა რეგისტრით დაწერილი ასოები, როგორც იგივე სიმბოლო
+
+#5
+print=(cotne) # პრინტს არ უნდა ტოლობა და უნდა ბრჭყალები
+
+a="5"
+b=10
+print(b+a) #str და int შორის მათემატიკური ოპერაცია ვერ შესრულდება
+
+Name=Cotne
+print=("name") #ასოების დიდი და პატარა რეგისტრი განსხვავდება case sensitivity
+
+name=("cotne") #ცვლადს არ სჭირდება ბრჩხილები და ბრჭყალები
