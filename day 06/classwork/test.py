af1ff8ad2bba22ab5b58ab98fa945aead1c4af64
cotnet <tevdorashvilicotne2010@gmail.com>
2024-10-09 18:41:04 +0400

    second commit

---
+++ day 06/classwork/test.py
@@ -0,0 +1,28 @@
+age = int(input("age: "))
+count_of_tens = age // 10
+print(count_of_tens)
+
+
+
+age = input("age: ")
+print(type(age))  
+age = float(age)  
+print(type(age))  
+
+
+name = input("your name: ")
+number = int(input("number: "))
+result = name * number
+print(result)
+
+
+x = 5  
+y = 2.0  
+result = x + y  
+print(result)  
+
+
+
+
+
+
