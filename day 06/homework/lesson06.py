63d2d4aa79d4f93ee11c6d94228c308b1af3e885
cotnet <tevdorashvilicotne2010@gmail.com>
2024-10-15 16:44:08 +0400

    second commit

---
+++ day 06/homework/lesson06.py
@@ -0,0 +1,56 @@
+#1
+age=int(input("your_age:"))
+age2= age + 10
+print(age2)
+print(float(age2))
+print(type(float(age2)))
+#2
+surname=input("your surname:")
+print(surname*100)
+#3
+word1="ბუ"
+word2="ხარი"
+print(word1+word2)
+word3="ice"
+word4="cream"
+print(word3+word4)
+word5="water"
+word6="melon"
+print(word5+word6)
+word7="butter"
+word8="fly"
+print(word7+word8)
+word9="sun"
+word10="flower"
+print(word9+word10)
+#4
+# მონაცემთა ტიპები აუცილებელია, რათა კოდი სწორად და ეფექტურად იმუშაოს. თითოეულ ტიპს აქვს 
+# თავისი როლი, რომელიც პროგრამაში სხვადასხვა ოპერაციის შესასრულებლად გამოიყენება:
+
+# 1. int (მთელი რიცხვი) - გამოიყენება რიცხვითი ოპერაციებში, სადაც საჭიროა მთელი რიცხვები.
+age = 25  # ასაკი (int)
+
+# 2. float (წილადი ან ათწილადი) - გამოიყენება წილადი ან ათწილადი რიცხვებისთვის
+height = 175.5  # სიმაღლე (float)
+
+# 3. str  - ყველაფერი რაც ბრჭყალებშია მოქცეული
+name = "Nino"  # სახელი (str)
+
+# 4. bool (ლოგიკური მნიშვნელობა) - გამოიყენება ორი შესაძლო მნიშვნელობისთვის: True ან False.
+is_student = True  # არის სტუდენტი (bool)
+
+# მონაცემთა ტიპები გვჭირდება იმისთვის, რომ სწორად დავალაგოთ და შევინახოთ ინფორმაცია. 
+# ეს ეხმარება პროგრამას იმუშაოს სწორად და იყოს უფრო კითხადი და მარტივი.
+
+#5
+name="cotne"
+print(type(name))
+age=14
+print(type(age))
+height=170.5
+print(type(height))
+True_False=is_student=False
+print(type(True_False))
+
+
+
