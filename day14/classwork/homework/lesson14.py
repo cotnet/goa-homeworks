63149792d49d19efe498f43d906cb7b80e080944
cotnet <tevdorashvilicotne2010@gmail.com>
2024-12-10 17:36:58 +0400

    second commit

--- day14/classwork/homework/lesson14.py
+++ day14/classwork/homework/lesson14.py
@@ -14,6 +14,37 @@ for i in range(5):
     favorite_animals.append(animal)
 print("პირველი ცხოველი:", favorite_animals[0])
 print("ბოლო ცხოველი:", favorite_animals[-1])
-new_animal = input(f"შეიყვანეთ სიის მეორე ცხოველის ახალი სახელი: ")
+new_animal = input("შეიყვანეთ სიის მეორე ცხოველის ახალი სახელი: ")
 favorite_animals[1] = new_animal
 print("განახლებული სია:", favorite_animals)
+#3
+
+numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
+every_third = numbers[2::3]
+reversed_list = numbers[::-1]
+middle_six = numbers[3:9]
+print("ყოველი მესამე ელემენტი:", every_third)
+print("სია უკუღმა:", reversed_list)
+print("შუა 6 ელემენტი:", middle_six)
+#4
+
+numbers = [x for x in range(1000, 10001) if x // 500 == 0]
+first_five = numbers[:5]
+every_third = numbers[::3]
+count_elements = len(numbers)
+print("პირველი ხუთი რიცხვი:", first_five)
+print("ყოველი მესამე რიცხვი:", every_third)
+print("საბოლოო სიის ელემენტების რაოდენობა:", count_elements)
+#5
+
+user_input = input("შეიყვანეთ 10 რიცხვი მძიმით გამოყოფილად: ")
+numbers = [int(x.strip()) for x in user_input.split(",")]
+first_three = numbers[:3]
+middle_four = numbers[3:7]
+is_last_ten = numbers[-1] == 10
+print("პირველი სამი რიცხვი:", first_three)
+print("შუა ოთხი ელემენტი:", middle_four)
+print("სიის ბოლო ელემენტი არის 10:", is_last_ten)
+
+
+
--- day14/classwork/text.py
+++ day14/classwork/text.py
@@ -1,3 +1,34 @@
+#1
 
-user=["vano","lika" , "Giorgi","nika","cotne","mari","nia","dato","mate","nini"]
-print(user[int(input("nuber 0-10"))])
+my_list = [10, 20, 30, 40, 50, 60, 70, 80, 90, 100]
+start = int(input("შეიყვანეთ პირველი რიცხვი დიაპაზონისთვის: "))
+end = int(input("შეიყვანეთ მეორე რიცხვი დიაპაზონისთვის: "))
+subset = my_list[start:end+1]
+print("დიაპაზონში არსებული ელემენტები:", subset)
+#2
+
+small_list = ["apple", "banana", "cherry", "date", "elderberry"]
+index = int(input("შეიყვანეთ ინდექსი (0-დან 4-მდე): "))
+if 0 <= index < len(small_list):
+    print("ამ ინდექსზე მდგომი ელემენტი არის:", small_list[index])
+else:
+    print("შეყვანილი ინდექსი არასწორია.")
+#3
+
+user_name = input("შეიყვანეთ თქვენი სახელი: ")
+my_name = "ცოტნე"
+result = user_name[:3] + my_name[-2:]
+print("შედეგი:", result)
+#4
+# Indexing:Indexing გამოიყენება სიის ან სტრიქონის კონკრეტულ პოზიციაზე მდგომი ერთი ელემენტის ამოსაღებად.მაგ., my_list[2] იძლევა მესამე ელემენტს.
+
+# Slicing:- Slicing საშუალებას გვაძლევს ამოვიღოთ ელემენტების დიაპაზონი სიის ან სტრიქონისგან.მაგ., my_list[2:5] იძლევა ელემენტებს მე-3-დან მე-5-მდე (3 ≤ i < 5).
+#5
+
+last_name = input("შეიყვანეთ თქვენი გვარი: ")
+
+reversed_last_name = last_name[::-1]
+
+print("თქვენი გვარი:", last_name)
+print("შებრუნებული გვარი:", reversed_last_name)
+
