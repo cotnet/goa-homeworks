bc06f8c099af16fc367191951c694429a69d35a6
cotnet <tevdorashvilicotne2010@gmail.com>
2025-02-10 21:31:18 +0400

    second commit

---
+++ day 22/homework/lesson22.py
@@ -0,0 +1,26 @@
+ #1
+def even_or_odd(number):
+    if number % 2 == 0:
+        return "ლუწი"
+    else:
+        return "კენტი"
+#2
+def your_name(name):
+    result="hallo"+ name
+    return result
+name1="Cotne"
+name2=your_name(name1)
+print(name2)
+
+name3="Nini"
+name4=your_name(name3)
+print(name4)
+#3
+def number_1(c,d):
+    result=str(c)+str(d)
+    return result
+
+numbers1=100
+numbers2=100
+number4=number_1(numbers2,numbers1)
+print(number4)
